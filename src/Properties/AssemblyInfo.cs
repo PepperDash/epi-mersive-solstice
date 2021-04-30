98cb5699957a88cc69fce382e6ccd13768193423
Nick Genovese <23391587+ngenovese11@users.noreply.github.com>
2021-04-30 13:05:06 -0400

    Update AssemblyInfo.cs

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,4 +5,5 @@
 [assembly: AssemblyProduct("MersiveEpi")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyVersion("1.0.0.*")]
-
+[assembly: AssemblyInformationalVersion("0.0.0-buildType-build#")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("0.0.0-buildType-build#")]
